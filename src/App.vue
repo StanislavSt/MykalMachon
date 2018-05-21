e415b9b0492fb1c483119f62c773473f5e1943c5
MykalMachon <mykalmachon@uvic.ca>
2018-05-21 00:08:03 -0700

    REDO! Redesign
    Redisned the basic parts of the site... didnt like the direction it was takling. Implemented CSS variables for coloring and made the layout a little bit better logically.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <div class="inner">
-     <!-- <Navigation /> -->
-      <Body />
+    <div id="inner">
+      <IntroCard />
     </div>
   </div>
 </template>
 
 <script>
-import Navigation from './components/Navigation.vue';
-import Body from './components/Body.vue';
+import IntroCard from './components/IntroCard.vue';
 
 export default {
   name: 'app',
   components: {
-    Navigation,
-    Body
+    IntroCard
   }
 };
 </script>
 
 <style>
 :root {
-  --blue: #b721ff;
+  --darkPrim: #455a64;
+  --lightPrim: #cfd8dc;
+  --prim: #607d8b;
+  --textIcons: #ffffff;
+  --accent: #ffc107;
+  --textPrim: #212121;
+  --textSec: #757575;
+  --divider: #bdbdbd;
 }
 
 * {
@@ -35,14 +39,69 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   display: flex;
   justify-content: center;
-  align-items: center;
   width: 100vw;
   height: 100vh;
-  background: #eeeeee;
+  background-color: var(--lightPrim);
 }
 
-.inner {
+button {
+  all: unset;
+  text-transform: uppercase;
+  padding: 6px 18px 6px 18px;
+  color: var(--textIcons);
+  background: var(--accent);
+  font-size: 1rem;
+  border-radius: 3px;
+  font-weight: normal;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  transition: all 0.2s ease-in-out;
+  letter-spacing: 1.5px;
+  cursor: pointer;
+}
+
+button:hover {
+  box-shadow: 0 9px 28px rgba(0, 0, 0, 0.25), 0 6px 10px rgba(0, 0, 0, 0.22);
+}
+
+button.light {
+  background-color: var(--lightPrim);
+  color: var(--textPrim);
+}
+
+button.dark {
+  background-color: var(--darkPrim);
+  color: var(--textIcons);
+}
+
+button.text {
+  background-color: unset;
+  border-radius: 0px;
+  color: var(--textIcons);
+  box-shadow: 0px 0px 0px;
+}
+
+button.text:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+#inner {
+  margin-top: 120px;
   display: flex;
   flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  width: 60%;
+}
+
+@media only screen and (max-width: 996px) {
+  #inner {
+    margin-top: 0px;
+    justify-content: flex-start;
+    width: 100%;
+  }
+  #introCard {
+    flex-direction: row;
+    min-width: unset;
+  }
 }
 </style>
---
+++ src/components/IntroCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="introCard">
+    <div id="summary" class='splitSum'>
+      <div class="top">
+        Mykal Machon
+        <p>
+          A Web Developer based in the Abbotsford, British Columbia, Canada. Working for Aprario Group and attending the University of the Fraser Valley for a Bachelors of Science in Computer Infromation Systems. 
+        </p>
+      </div>
+      <div class="bottom">
+        <button class="dark">Contact</button>
+        <button class="text">follow</button>
+      </div>
+    </div>
+    <div id="picture" class='splitPic'>
+      <img id='me' src="../assets/me.jpg" alt="Mykal Machon" >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IntroCard',
+  methods: {}
+};
+</script>
+
+<style scoped>
+#introCard {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: nowrap;
+  width: 100%;
+  min-width: 868px;
+  max-width: 1000px;
+  background-color: var(--prim);
+  color: var(--textIcons);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s ease-in-out;
+}
+
+div.splitSum {
+  width: 50%;
+  padding: 20px;
+  font-size: 4em;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+p {
+  font-size: 1rem;
+  font-weight: normal;
+}
+
+div.splitPic {
+  width: 50%;
+}
+
+div.splitPic img {
+  width: 100%;
+  height: 100%;
+}
+
+@media only screen and (max-width: 996px) {
+  #introCard {
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    min-width: 0px;
+  }
+
+  #picture {
+    display: none;
+  }
+}
+</style>
