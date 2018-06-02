762a0f59b189657aebb1b7ac340e495ff0a6c4e0
Mykal Machon <7844994+MykalMachon@users.noreply.github.com>
2018-06-02 13:32:22 -0700

    Updated SkillsCard
    Updated the skills card to match design revision

--- src/components/SkillsCard.vue
+++ src/components/SkillsCard.vue
@@ -1,37 +1,13 @@
 <template>
   <div id="skillsCard">
-    <div class='headingTop'>
-      <h2>Skills</h2>
-      <p>What im good at, getting better at, and even what I'm not good at!</p>
+    <div class="heading">
+      <div class="head-text">
+        <h2>Skills and Focuses</h2>
+      </div>
+      <img src="../assets/gaus.jpg" alt="gaussian Blur">
     </div>
-    <div class="skills">
-      <p>Front End Web Development</p>
-      <div class="badge-container">
-        <div class="badge">
-          <i class="devicon-html5-plain"></i> HTML 5
-        </div>
-        <div class="badge">
-          <i class="devicon-css3-plain"></i> CSS3
-        </div>
-        <div class="badge">
-          <i class="devicon-javascript-plain"></i> ES6 Javascript
-        </div>
-        <div class="badge">
-          <i class="devicon-vuejs-plain"></i> Vue.js
-        </div>
-      </div>
-      <p>Back End Web Development</p>
-      <div class="badge-container">
-        <div class="badge">
-          <i class="devicon-nodejs-plain"></i> Node.js
-        </div>
-        <div class="badge">
-          <i class="devicon-express-original"></i> Express
-        </div>
-        <div class="badge">
-        <i class="devicon-mongodb-plain"></i> MongoDB
-        </div>
-      </div>
+    <div class="content">
+
     </div>
   </div>
 </template>
@@ -48,58 +24,72 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+  align-items:flex-start;
   flex-wrap: nowrap;
   width: 100%;
   min-width: 868px;
   max-width: 1000px;
   background-color: var(--textIcons);
   color: var(--textPrim);
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0px 15px 28px rgba(0,0,0,0.15);
   transition: all 0.3s ease-in-out;
   border-radius: 5px;
-  padding: 20px;
+  z-index: -2;
+
 }
 
-h2 {
-  font-size: 2rem;
+img{
+  display: block;
+  height: auto;
+  min-height: 200px;
+  max-width: 100%;
+  filter: blur(40px);
+  z-index: -1;
+}
+
+div.heading{
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: inline-block;
+  position: relative;
+  max-height: 200px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.head-text{
+  position: absolute;
+  text-align: left;
+  width: 100%;
+  height: 100%;
+  padding: 50px;
+  z-index: 0;    
+}
+
+.head-text:before {
+   content: '';
+   display: inline-block;
+   height: 100%;
+   vertical-align: middle;
+}
+
+.head-text h2{
+  display: inline-block;
+  margin: 0px;
+  font-size: 2em;
   font-weight: bold;
+  color: white;
+  text-shadow: 0 2px 6px rgba(0,0,0,0.15);
 }
 
-.headingTop h2 {
-  margin-bottom: 0px;
-}
-.headingTop p {
-  margin-top: 0px;
-  color: var(--textSec);
-}
-div.badge-container {
-  display: flex;
-  flex-direction: flex-start;
-  flex-wrap: wrap;
+.content{
+  height: 200px;
 }
 
-div.badge {
-  padding: 7px 16px 7px 16px;
-  border-radius: 24px;
-  background: var(--accent);
-  color: black;
-  margin-right: 20px;
-  white-space: nowrap;
-  cursor: pointer;
-  transition: all 0.3s cubic-bezier(0.11, 1.5, 0.71, 1.5);
-}
-
-div.badge:hover {
-  transform: scale(1.1);
-}
-
-.skills p {
-  font-weight: bold;
-}
 
 @media only screen and (max-width: 996px) {
   #skillsCard {
-    padding: 40px;
     display: flex;
     min-width: 0px;
     border-radius: 0px;
