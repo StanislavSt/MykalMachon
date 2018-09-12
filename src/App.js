d090b791204f68e623a3682732a330e22eda5434
Mykal Machon <mykalmachon@gmail.com>
2018-09-11 19:58:19 -0700

    Testing Media Queries

--- src/App.js
+++ src/App.js
@@ -67,6 +67,10 @@ const AppContainer = styled.div`
     animation-fill-mode: forwards;
     background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%232d84ff' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
   }
+
+  @media (max-width: 925px) {
+    justify-content: flex-start;
+  }
 `;
 
 const Card = styled.div`
@@ -80,6 +84,11 @@ const Card = styled.div`
   animation: ${loadInDown} 0.8s ease-in-out;
   animation-fill-mode: forwards;
   box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.12), 0px 2px 8px rgba(0, 0, 0, 0.3);
+
+  @media (max-width: 925px) {
+    margin-top: 30%;
+    width: 90%;
+  }
 `;
 
 export default App;
