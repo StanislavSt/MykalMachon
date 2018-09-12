a79709461001951124eb039d793b1daa43f73585
Mykal Machon <mykalmachon@gmail.com>
2018-09-11 19:45:54 -0700

    Added Some Basic Content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ class App extends Component {
   render() {
     return (
       <AppContainer>
-        <AppBackground />
+        <Card>
+          <h1>Mykal Machon</h1>
+          <p>
+            A Web Developer and Student at the university of the fraser valley.
+          </p>
+        </Card>
       </AppContainer>
     );
   }
@@ -14,7 +19,7 @@ class App extends Component {
 const loadIn = keyframes`
   from{
     opacity: 0;
-    transform: translateY(5px);
+    transform: translateY(10px);
   }
 
   to{
@@ -24,6 +29,7 @@ const loadIn = keyframes`
 `;
 
 const AppContainer = styled.div`
+  position: relative;
   width: 100vw;
   height: 100vh;
   overflow: hidden;
@@ -31,21 +37,29 @@ const AppContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-const AppBackground = styled.div`
-  position: absolute;
-  top: 0px;
-  bottom: 0px;
-  right: 0px;
-  left: 0px;
   background-color: #ffffff;
   z-index: -1;
-  opacity: 0;
-  transform: translateY(5px);
-  animation: ${loadIn} 1s ease-in-out;
-  animation-fill-mode: forwards;
-  background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%232d84ff' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    animation: ${loadIn} 1s ease-in-out;
+    animation-fill-mode: forwards;
+    background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%232d84ff' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+  }
+`;
+
+const Card = styled.div`
+  max-width: 500px;
+  z-index: 1;
+  width: 50%;
+  background-color: #ffffff;
+  border-radius: 4px;
+  padding: 22px;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.12), 0px 2px 8px rgba(0, 0, 0, 0.3);
 `;
 
 export default App;
