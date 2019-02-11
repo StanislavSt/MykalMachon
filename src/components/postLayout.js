bc742fea57ccbfaa3ecd041165bf2ea9233244fc
Mykal Machon <mykalmachon@gmail.com>
2019-02-11 13:06:45 -0800

    Page Generation with gatsby-node.js
    + Generates pages using gatsby-node.js and creating a page for each md file in the posts folder

---
+++ src/components/postLayout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Layout from './layout';
+
+export default class PostLayout extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>Post Layout</h1>
+      </Layout>
+    );
+  }
+}
