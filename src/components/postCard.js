0890d8c03a9861c406e241c499530a38ff53ed62
Mykal Machon <mykalmachon@gmail.com>
2019-02-11 12:45:45 -0800

    Update postCard.js

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -9,7 +9,7 @@ const PostCard = ({ title, excerpt, slug, date, readTime }) => (
     <h4>
       {date} - <span>{readTime} Minutes</span>
     </h4>
-    <Link to={`/posts/${slug}`}>
+    <Link to={`/posts${slug}`}>
       <h2>{title}</h2>
     </Link>
     <p>{excerpt}</p>
