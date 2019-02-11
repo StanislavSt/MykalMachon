5cf3b5d5f20a62d1348e20a571bf44c9e06a5ca3
Mykal Machon <mykalmachon@gmail.com>
2019-02-11 14:09:26 -0800

    Simple Contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,16 +3,28 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+import FormStyles from '../components/styles/FormStyles';
+
 const Contact = () => (
   <Layout>
     <SEO title="Contact" />
     <h1>Contact</h1>
-    <p>There are tonnes of ways to get a hold of me, but here are a few: </p>
-    <ul>
-      <li>Github</li>
-      <li>Twitter</li>
-      <li>E-Mail</li>
-    </ul>
+    <p>
+      If you want to get a hold of me through email, use the form bellow but if
+      you're more prone to social media, you can reach out on{' '}
+      <a href="https://twitter.com/mykalmachon">twitter</a> as well!
+    </p>
+    <FormStyles>
+      <form action="POST">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" placeholder="John Smith" />
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" placeholder="john@something.com" />
+        <label htmlFor="message">Message</label>
+        <textarea name="message" placeholder="Your message goes here" />
+        <button className="submit">Submit</button>
+      </form>
+    </FormStyles>
   </Layout>
 );
 
