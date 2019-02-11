e49b1ad1dde10723600eac6487179da8d2d3dc83
Mykal Machon <mykalmachon@gmail.com>
2019-02-11 15:03:38 -0800

    Updating PWA Features
    + Updating PWA content to contain relevant information.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Mykal Machon`,
     description: `I am a full time CIS Student at UFV who develops webapps in his spare time.`,
-    author: `@gatsbyjs`
+    author: `@mykalmachon`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,13 +26,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Mykal Machon`,
+        short_name: `Mykal's Blog`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#9D46F4`,
+        theme_color: `#9D46F4`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/icon.png` // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-styled-components',
