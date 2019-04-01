074fb95f59f22ff59dd06acb8f8f4cc098be60b9
Mykal Machon <mykalmachon@gmail.com>
2019-04-01 15:46:08 -0700

    updating the About page
    + This is a rewrite of the about page to have a little bit more information

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,18 +8,39 @@ import SEO from '../components/seo';
 const Contact = () => (
   <Layout>
     <SEO title="About" />
-    <h1>About</h1>
-    <p>Well, what do you want to know about?</p>
-    <h2>Mykal Machon</h2>
+    <h1>
+      Hi! I'm Mykal{' '}
+      <span role="img" aria-label="wave">
+        👋
+      </span>
+    </h1>
+    <p>Nice to e-meet you!</p>
     <p>
-      I'm a university student at the University of the Fraser Valley with a
-      passion for web applications and web technologies.
+      I'm a university student and software developer from British Columbia,
+      Canada. When I'm not studying, I'm either working at{' '}
+      <a href="https://aprario.com">Aprario Group</a>, a community focused
+      investment company, or tinkering with one of my many{' '}
+      <a href="https://github.com/mykalmachon">side projects. </a>
     </p>
-    <h2>This website</h2>
     <p>
-      This website was made using Gatsby.js and is going to act as my personal
-      blog and side project for the time being. All of the code can be found{' '}
-      <a href="https://github.com/MykalMachon/MykalMachon">here on github</a>
+      Im currently completing a Bachelor in Computer Information Sciences with a
+      focus in Software Development at the{' '}
+      <a href="https://ufv.ca">University of the Fraser Valley</a>. I'm in my
+      third year and plan to complete my degree by the summer of 2020.
+    </p>
+    <p>
+      I'm also available for hire to work individually on small software
+      projects or in teams as needed. I specialize in engineering web
+      applications that solve business needs. In the past I've worked on :
+      <ul>
+        <li>Financial dashboards</li>
+        <li>Automated reporting systems </li>
+        <li>System integrations </li>
+        <li>Company websites</li>
+      </ul>
+      If you'd like to work with me on a project feel free to reach out through
+      my contact page or through social media (a Twitter DM will probably the
+      best way to do this)
     </p>
   </Layout>
 );
